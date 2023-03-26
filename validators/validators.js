7213e87edcb04ca90fd10328b1ebe42da610ad55
LudmilaVP <liudmila.lupandina@yandex.ru>
2023-03-27 02:11:06 +0300

    fin

--- validators/validators.js
+++ validators/validators.js
@@ -18,7 +18,7 @@ const signupValidator = celebrate({
 
 const updateUserValidator = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
   }),
 });
